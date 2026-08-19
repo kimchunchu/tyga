423429a41b052a235045966bea42058e08b94110
kimchunchu <dlwodud821@gmail.com>
2026-08-20 03:14:33 +0900

    feat: add simple response

--- include/http/response.hpp
+++ include/http/response.hpp
@@ -9,12 +9,23 @@ struct ResponseHeader {
   std::string value;
 };
 
-struct Response {
-  int status_code;
-  std::string status_text;
-  std::vector<ResponseHeader> headers;
-  std::string body;
+class Response {
+public:
+  Response(int status_code, std::string status_text,
+           std::vector<ResponseHeader> headers, std::string body);
+  static Response ok(std::string body);
+  static Response not_found();
+  static Response method_not_allowed();
+  static Response bad_request();
+  static Response payload_too_large();
+  Response &header(std::string name, std::string value);
+  std::string serialize();
+
+private:
+  int status_code_;
+  std::string status_text_;
+  std::vector<ResponseHeader> headers_;
+  std::string body_;
 };
 
-std::string serialize_response(const Response &response);
 } // namespace tyga::http
--- src/http/response.cpp
+++ src/http/response.cpp
@@ -3,17 +3,51 @@
 #include <string>
 
 namespace tyga::http {
-std::string serialize_response(const Response &response) {
+Response::Response(int status_code, std::string status_text,
+                   std::vector<ResponseHeader> headers, std::string body)
+    : status_code_(status_code), status_text_(status_text), headers_(headers),
+      body_(body) {};
+
+Response Response::not_found() { return {400, "Not Found", {}, "Not Found"}; }
+
+Response Response::method_not_allowed() {
+  return Response{405, "Method Not Allowed", {}, "Method Not Allowed"};
+}
+
+Response Response::ok(std::string body) {
+  return Response{200, "OK", {}, body};
+}
+
+Response Response::bad_request() {
+  return Response{400, "Bad Request", {}, "Bad Request"};
+}
+
+Response Response::payload_too_large() {
+  return Response{413, "Payload Too Large", {}, "Payload Too Large"};
+}
+
+Response &Response::header(std::string name, std::string value) {
+  headers_.push_back({
+      std::move(name),
+      std::move(value),
+  });
+
+  return *this;
+}
+
+std::vector<ResponseHeader> Response::headers() { return headers_; }
+
+std::string Response::serialize() {
   std::string result;
   result += "HTTP/1.1";
   result += " ";
-  result += std::to_string(response.status_code);
+  result += std::to_string(status_code_);
   result += " ";
-  result += response.status_text;
+  result += status_text_;
   result += "\r\n";
 
   bool has_content_length = false;
-  for (const auto &header : response.headers) {
+  for (const auto &header : headers()) {
     if (iequals(header.name, "Content-Length")) {
       has_content_length = true;
     }
@@ -26,12 +60,12 @@ std::string serialize_response(const Response &response) {
 
   if (!has_content_length) {
     result += "Content-Length: ";
-    result += std::to_string(response.body.size());
+    result += std::to_string(body_.size());
     result += "\r\n";
   }
 
   result += "\r\n";
-  result += response.body;
+  result += body_;
 
   return result;
 }
